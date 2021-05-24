f780d553228cfff3a8c123f978bc899cf3d13807
Vick Bay <levicking@hotmail.com>
2021-05-24 22:51:39 +0400

    adding maps functionality

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -6,12 +6,16 @@ import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
 
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
-
+  console.log(keyword);
   useEffect(() => {
     setSearchKeyword(keyword);
   }, [keyword]);
@@ -20,6 +24,7 @@ export const Search = () => {
     <SearchContainer>
       <Searchbar
         placeholder="Search for a location"
+        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword);
